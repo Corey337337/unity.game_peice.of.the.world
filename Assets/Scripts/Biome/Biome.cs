3ed6f16be58eb9676fce097d20d4260fe05ac692
Corey337337 <egorvasilevic95@gmail.com>
2026-08-25 16:52:09 +0300

    Getting started

---
+++ Assets/Scripts/Biome/Biome.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public enum BiomeType
+{
+    Plains,
+    Forest,
+    Desert,
+    Snow,
+    River,
+    Ocean
+}
---
+++ Assets/Scripts/Blocks/Block.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public string blockName;
+
+    public int blockID;
+
+    public BiomeType biomeType;
+
+
+    public void generateBlock(int id)
+    {
+        blockID = id;
+        // Additional logic to generate the block based on the ID can be added here
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
