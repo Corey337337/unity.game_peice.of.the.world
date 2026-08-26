7ee22a919fe324e95bceb6dd175113baf037175a
Corey337337 <egorvasilevic95@gmail.com>
2026-08-26 14:02:29 +0300

    changed generation script

--- Assets/Scripts/Blocks/Generation.cs
+++ Assets/Scripts/Blocks/Generation.cs
@@ -1,28 +1,42 @@
 using UnityEngine;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
 
 public class Generation : MonoBehaviour  //экспериментальный скрипт генерации блоков
 {
    
-
     // пока что один префаб который назначается в инспекторе
     // потом будет рандомная генерация из списка (шансы и прочее прошипу позже)
-    public Block blockPrefab; 
 
-    public void GeneratePlace()
+    //public Block blockObject; //заменить на лист блоков и потом рандомить их появление
+    public List<Block> possibleBlocks;//вот
+    public Chunk chunkObject;
+
+    public GameObject playerObject;
+
+    public Block GenerateBlocksInChunk()
     {
-       for (int i = 0; i < 5; i++)
+        int randomBlock = Random.Range(0, possibleBlocks.Count);
+        return possibleBlocks[randomBlock];
+    }
+
+    public void GenerateChunk()
+    {
+       for (int i = 0; i < chunkObject.width; i++)
        {
-            for (int j = 0; j < 5; j++)
+            for (int j = 0; j < chunkObject.height; j++)
             {
-                Instantiate(blockPrefab, new Vector3(i, j, 0), Quaternion.identity);
+                Instantiate(GenerateBlocksInChunk(), new Vector3(i, j, 0), Quaternion.identity);
             }
        }
     }
 
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GeneratePlace();
+        GenerateChunk();
     }
 
     // Update is called once per frame
