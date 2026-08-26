9a9f9991a7586dc9137f7d07319de75b875f1bca
Corey337337 <egorvasilevic95@gmail.com>
2026-08-26 14:00:35 +0300

    started working with chunks

--- Assets/Scripts/Chunks/Chunk.cs
+++ Assets/Scripts/Chunks/Chunk.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Chunk : MonoBehaviour
 {
+    public int height;
+    public int width;
 
-
+    public List<Block> blocksInChunk;//не знаю нужно ли вообще чанку знать о своих блоках. это какбудто бессмысленно
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
