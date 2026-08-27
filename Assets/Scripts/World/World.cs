77c13f3b9ebabea7b26362286e0c181506f16571
Corey337337 <egorvasilevic95@gmail.com>
2026-08-27 16:27:00 +0300

    added map generation logic

--- Assets/Scripts/World/World.cs
+++ Assets/Scripts/World/World.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.Audio;
 
 public class World : MonoBehaviour
@@ -11,35 +13,109 @@ public class World : MonoBehaviour
     public Chunk chunk;
     public Generator generator;
 
-    public void CheckChunks()
-    {
+    private Vector2Int CurrentChunk;
+    private Vector2Int PreviousChunk;
+    private List<Chunk> ListChunks = new List<Chunk>();
 
+
+
+    public Vector2Int GetCurrentChunk()
+    {
+        int x = Mathf.FloorToInt(player.transform.position.x / chunk.width);
+        int y = Mathf.FloorToInt(player.transform.position.y / chunk.height);
+
+        return new Vector2Int(x, y);
     }
 
+
+    public List<Vector2Int> GetNeighboursCords()
+    {
+        CurrentChunk = GetCurrentChunk();
+        List<Vector2Int> neighboursCords = new List<Vector2Int>();
+
+        for (int dx = -1; dx <= 1; dx++)
+        {
+            for (int dy = -1; dy <= 1; dy++)
+            {
+                var cord_x = CurrentChunk.x + dx;
+                var cord_y = CurrentChunk.y + dy;
+
+                neighboursCords.Add(new Vector2Int(cord_x, cord_y));
+
+            }
+        }
+        return neighboursCords;
+    }
+
+    public bool IsChunkExists(Vector2Int position)
+    {
+        for (int i = 0; i < ListChunks.Count; i++)
+        {
+            if (ListChunks[i].chunkPosition == position)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void SvuazZveno()//временное название
+    {
+        List<Vector2Int> spisok = GetNeighboursCords();
+
+        foreach (Vector2Int cord in spisok)
+        {
+            if (!IsChunkExists(cord))
+            {
+                var c = Instantiate(chunk, new Vector3(cord.x * chunk.width, cord.y * chunk.height, 0), Quaternion.identity, transform);
+                c.chunkPosition = cord;
+                c.GenerateChunk(generator);
+
+                ListChunks.Add(c);
+            }
+        }
+    }
+
+   
     public void BuildWorld()
     {
         for (int i = 0; i < visibleWorldSize; i++)
         {
             for (int j = 0; j < visibleWorldSize; j++)
             {
-                var a = Instantiate(chunk, new Vector3(i * chunk.width, j * chunk.height, 0), Quaternion.identity, transform);
-                a.GenerateChunk(generator);
+                var c = Instantiate(chunk, new Vector3(i * chunk.width, j * chunk.height, 0), Quaternion.identity, transform);
+
+                c.chunkPosition = new Vector2Int(i, j);
+
+                c.GenerateChunk(generator);
+
+                ListChunks.Add(c);
             }
         }
     }
 
     
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         BuildWorld();
         player.GetComponent<PlayerLocation>().PlayerSpawn();
+
+        CurrentChunk = GetCurrentChunk();
+        PreviousChunk = CurrentChunk;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2Int newChunk = GetCurrentChunk();
+
+        if (newChunk != CurrentChunk)
+        {
+            PreviousChunk = CurrentChunk;
+            CurrentChunk = newChunk;
+
+            SvuazZveno();
+          
+        }
     }
 }
