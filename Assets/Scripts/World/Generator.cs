9a1fff5e41e30deec2e06a147dda68ace8816864
Corey337337 <egorvasilevic95@gmail.com>
2026-09-19 17:17:01 +0300

    added Perlin Noise logic

--- Assets/Scripts/World/Generator.cs
+++ Assets/Scripts/World/Generator.cs
@@ -7,10 +7,36 @@ public class Generator : MonoBehaviour
 
     public List<Block> possibleBlocks;
 
-    public Block GenerateBlocksInChunk()
+    [Header("Настройки шума")]
+    public float scale;
+    public Vector2 offset;
+
+
+    private void Awake()
     {
-        int randomBlock = Random.Range(0, possibleBlocks.Count);
-        return possibleBlocks[randomBlock];
+        offset = new Vector2(Random.Range(0, 10000), Random.Range(0, 10000));
     }
 
+    public Block GenerateBlocksInChunk(int globalX, int globalY)
+    {
+        //int randomBlock = Random.Range(0, possibleBlocks.Count);
+        //return possibleBlocks[randomBlock];
+        float noiseValue = Mathf.PerlinNoise((globalX + offset.x) * scale,(globalY + offset.y) * scale);
+
+        return GetBlockByNoise(noiseValue);
+    }
+
+
+    public Block GetBlockByNoise(float value)
+    {
+        if (value < 0.3)
+            return possibleBlocks[1];
+        else if (value < 0.4)
+            return possibleBlocks[2];
+        else if (value < 0.75)
+            return possibleBlocks[0];
+        else
+            return possibleBlocks[3];
+
+    }
 }
